f2f516454a401c01fd491d4b916927130cdc3b04
Gérson Porciúncula <gerson.porciuncula@atacadotreichel.com.br>
2019-11-28 15:13:21 -0300

    versao 3

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask
+# Importando a biblioteca
+# importando instância da classe Flask
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello World! Gerson", 200
+app = Flask(__name__) #Instancia da classe Flask
 
-app.run()
+
+@app.route("/") #Decorador sempre antes de função
+#aplica o metodo antes da função
+#faz a rota da pagina
+def index():
+	return "Hello World"
+
+
+#verifica se está executando
+if __name__ == "__main__":
+	app.run()
+
+
